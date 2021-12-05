1dd8492b6beb163512c55718d734e9f37be00260
Cordell <cordellbonnieux@gmail.com>
2021-12-05 09:53:47 -0800

    Added date reading logic.

--- HotelServer.java
+++ HotelServer.java
@@ -3,29 +3,84 @@ import java.net.*;
 
 public class HotelServer {
 	public static void main(String[] args) {
-		final int PORT = 420;
+		
+		final int PORT = 1181;
+		String user;
+		DataInputStream in;
+		DataOutputStream out;
+		
+		/*
+		 * RUN THE SERVER
+		 */
 		try (ServerSocket server = new ServerSocket(PORT)) {
 			System.out.println("Waiting for client to connect...");
 			while (true) {
-				try (Socket s = server.accept()) { // wait for next client
-					DataInputStream in = new DataInputStream(s.getInputStream());
-					DataOutputStream out = new DataOutputStream(s.getOutputStream());
+				/*
+				 * WAIT FOR A CLIENT TO CONNECT
+				 */
+				try (Socket s = server.accept()) {
+					in = new DataInputStream(s.getInputStream());
+					out = new DataOutputStream(s.getOutputStream());
 					out.writeUTF("You have connected to the server");
 					out.flush();
 					boolean done = false;
+					String text;
 					
 					while (!done) {
-						String text = in.readUTF();
-						done = text.equalsIgnoreCase("quit");
-						out.writeUTF("echo: " + text);
-						out.flush();
+						/*
+						 *  USER LOGIN
+						 */
+						text = in.readUTF().toUpperCase();
+						if (text.contentEquals("USER")) {
+							System.out.print("USER selected");
+							text = in.readUTF();
+							user = text;
+							out.writeUTF("Hello, " + text);
+							out.writeUTF("Enter RESERVE to reserve dates...");
+							out.flush();
+							/*
+							 * RESERVE DATES
+							 */
+							text = in.readUTF().toUpperCase();
+							if (text.contentEquals("RESERVE")) {
+								System.out.println("RESERVE selected"); 
+								// check date 1
+								int start = in.readInt();
+								if (start > 30 || start < 1) {
+									out.writeUTF("You've entered an invalid date.");
+								}
+								// check date 2
+								int end = in.readInt();
+								if (end < start || end == start || end > 31) {
+									out.writeUTF("You've entered an invalid date.");
+								}
+								
+							} else {
+								/*
+								 * NO RESERVE
+								 */
+								out.writeUTF("Did not recognize input, please enter RESERVE, followed by two valid dates.");
+							}
+
+						} else {
+							/*
+							 * NO USER
+							 */
+							out.writeUTF("Did not recognize input, please enter USER followed by a username");
+						}
 					}
 				} catch (IOException e ) {
+					/*
+					 * CLIENT COULD NOT CONNECT
+					 */
 					e.printStackTrace();
 					System.out.println("Client could not connect.");
-				}
+				}	
 			}
 		} catch (IOException e ) {
+			/*
+			 * SERVER FAILED TO START
+			 */
 			e.printStackTrace();
 			System.out.println("IOException @ main serversocket");
 		}
